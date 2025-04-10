0b3a9f070c4c0ef2becae774f397d48bc10fc6d9
pietroperona <p.perona@koodit.it>
2025-04-10 18:28:07 +0200

    Implement Google Tag Manager for pageview tracking and clean up meta tags in document

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,31 @@
 // pages/_app.js
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import '../styles/globals.css'
-import GoogleTagManager from '../components/GoogleTagManager'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <GoogleTagManager />
-      <Component {...pageProps} />
-    </>
-  )
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      // Invia un evento pageview a Google Tag Manager
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({
+        'event': 'pageview',
+        'page': url
+      });
+    };
+
+    // Aggiungi l'event listener per il cambio pagina
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // Rimuovi l'event listener quando il componente viene smontato
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return <Component {...pageProps} />
 }
 
 export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -26,28 +26,12 @@ export default function Document() {
         />
         {/* End Google Tag Manager */}
         
-        {/* Favicon e icone */}
+        {/* Resto del codice esistente */}
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="canonical" href="https://biella.aimeetup.it" />
-        
-        {/* Meta tag per crawler AI */}
-        <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1" />
-        <meta name="googlebot" content="index, follow" />
-        <meta name="ai-index" content="allow" />
-        <meta name="format-detection" content="telephone=no" />
-        
-        {/* Meta tag per SEO avanzato */}
-        <meta name="geo.region" content="IT-BI" />
-        <meta name="geo.placename" content="Biella" />
-        <meta name="geo.position" content="45.5629;8.0583" />
-        <meta name="ICBM" content="45.5629, 8.0583" />
-        
-        {/* Supporto multi-lingua per il futuro */}
-        <link rel="alternate" hreflang="it" href="https://biella.aimeetup.it" />
-        <link rel="alternate" hreflang="x-default" href="https://biella.aimeetup.it" />
       </Head>
       <body>
         {/* Google Tag Manager (noscript) */}
